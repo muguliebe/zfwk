ee54455d50873be2cb1b59fedb5be84860ac9953
zany <muguliebe@gmail.com>
2021-01-05 13:58:44 +0900

    init

---
+++ src/main/kotlin/zfwk/proto/ZFwkApplication.kt
@@ -0,0 +1,11 @@
+package zfwk.proto
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ZFwkApplication
+
+fun main(args: Array<String>) {
+    runApplication<ZFwkApplication>(*args)
+}
---
+++ src/test/kotlin/com/egstep/zfwk/ZfwkTest.kt
@@ -0,0 +1,13 @@
+package com.egstep.zfwk
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ZfwkTest{
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
