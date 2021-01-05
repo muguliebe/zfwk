bb38e8a425e5643d5e089fd7316d147b6391bf29
zany <muguliebe@gmail.com>
2021-01-05 14:54:25 +0900

    add test target class properly

--- src/test/kotlin/zfwk/ZfwkTest.kt
+++ src/test/kotlin/zfwk/ZfwkTest.kt
@@ -3,7 +3,9 @@ package zfwk
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
+@SpringBootTest(
+    classes = [ZfwkTest::class]
+)
 class ZfwkTest{
 
     @Test
